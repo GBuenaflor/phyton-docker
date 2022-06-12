54bc1622ade78be07da950d04b32ecc6d5419404
Gerardo Buenaflor <b_junx2008@hotmail.com>
2022-06-12 21:00:26 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,21 +1,26 @@
+# Python Flask build_spec.yaml 
+# POC Develop By GB Buenaflor and isRAEL Casisdsid
+# OKE , MySQL ,Terraform ,DevOPS
+
 from flask import Flask
 import mysql.connector
 
 mydb = mysql.connector.connect(
   host="10.0.50.252",
-  user="OKEDBADmin",
-  password='YOURPASSWORDHERE',
+  user="USERDATA",
+  password='PASsWORDHERE',
   database="UPDatabase",
-  auth_plugin='mysql_native_password'
-) 
+  port=3306
+  #auth_plugin='mysql_native_password'
+)
 
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM Table01")
 dbResult = mycursor.fetchone()
 print('MySQLDB Data: ')
 print(dbResult)
- 
-  
+
+
 app = Flask(__name__)
 
 count = 0
@@ -23,7 +28,7 @@ count = 0
 def index():
     global count
     count += 1
-    return 'Python with Flask Website.' + str(count) 
+    return 'Python with Flask Website.'+ 'MySQL Data: ' + str(dbResult) + str(count)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
