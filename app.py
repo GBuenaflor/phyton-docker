859c7a2e18ea37eb07305449b7ef5e82af39b400
Gerardo Buenaflor <b_junx2008@hotmail.com>
2022-06-12 16:57:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
 
 from flask import Flask
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="10.0.50.252",
-  user="OKEDBADminPOC",
-  password='noP33swOrDinthi$$',
-  database="UPDatabase",
-  auth_plugin='mysql_native_password'
-) 
-
-#mycursor = mydb.cursor()
-#mycursor.execute("SELECT * FROM Table01")
-#dbResult = mycursor.fetchone()
-#print('MySQLDB Data: ')
-#print(dbResult)
-
 app = Flask(__name__)
+
 count = 0
+
 @app.route('/')
 def index():
-    global count    
-    count += 1 
+    global count
+    count += 1
     return 'Python with Flask Website - OKE,MySQL,Terraform,DevOPS. GBuenaflor/iSRAel : ' + str(count) 
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
