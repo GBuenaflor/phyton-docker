c41877443f81356d19e86c36e2733a88a9dd407c
Gerardo Buenaflor <b_junx2008@hotmail.com>
2022-07-04 00:29:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ app = dash.Dash(__name__, external_stylesheets=[
 #server = app.server
 server = '0.0.0.0'
 
-# Talisman(server) not working
-# sslify = SSLify(server)
+ # Talisman(server) not working
+ # sslify = SSLify(server)
 
 app.config.suppress_callback_exceptions = True
 app.css.config.serve_locally = True
